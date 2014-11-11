d456ba93c04af3150405af0368367b95f9989a05
MasaruMiyamoto <miyamotomasaru@miyamotoyuu-no-MacBook-Air.local>
2014-11-11 14:07:04 +0900

    Initial Commit

---
+++ Draw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Draw
+//
+//  Created by MasaruMiyamoto on 2014/11/11.
+//  Copyright (c) 2014年 MasaruMiyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Draw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Draw
+//
+//  Created by MasaruMiyamoto on 2014/11/11.
+//  Copyright (c) 2014年 MasaruMiyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
